b5ae1fbea2d03976d06c1616a1a18bb74367aaac
Corim <Corim@users.noreply.github.com>
2018-08-20 20:33:33 -0500

    Added comments

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { fromEvent, of } from 'rxjs';
-import { filter, map, scan, merge, tap } from 'rxjs/operators';
+import { filter, map, scan, merge } from 'rxjs/operators';
 
 import ReactStreamWrapper from '../architecture/ReactStreamWrapper'
 
+// Normal React Component
 const Terminal = (props) => {
   return (
     <div>
@@ -29,47 +30,54 @@ const Terminal = (props) => {
   )
 }
 
+// Wrapper around React Component to inject streams (injects streams as props into the component)
 export default (props) => {
 
+  /***************** Input Listeners **********************/
+
+  // Maps each input to a function
   const mousedown = fromEvent(document, 'mousedown')
     .pipe(
       filter(e => (e.target.id !== 'linusTerminal' && e.target.id !== 'outerLinusTerminal')),
-      // Map to a function which returns false
-      map(e => () => false)
-
+      map(e => (state) => false)
     )
 
+  // Maps each input to a function
   const esc = fromEvent(document, 'keydown')
     .pipe(
       filter(e => e.key === 'Escape'),
-      // Map to a function which returns false
-      map(e => () => false)
+      map(e => (state) => false)
     )
 
+  // Maps each input to a function
   const toggle = fromEvent(document, 'keypress')
     .pipe(
       filter(e => e.key==='b' && e.ctrlKey===true),
-      // Map to a function which returns opposite of current state
       map(e => (state) => !state)
     )
 
+
+  // Creates the toggle state by calling the functions from the streams above.
   const toggled = toggle
     .pipe(
       merge(esc),
       merge(mousedown),
+      // Start with toggled false. Call each function passing the previous state in
       scan((state, next) => next(state), false),
     )
 
   /*************************** Functions **************************/
 
+  // Handles changes to the text input box
   const handleText = (text) => {
     return console.log(text)
   }
 
   const streams = {
     toggled: toggled,
-    handleText: of(handleText) // Needs to be a stream
+    handleText: of(handleText) // Passes function in as a single item stream
   }
 
+  // Wraps the react component and adds the streams as props
   return ReactStreamWrapper(streams, Terminal, props)
 }
